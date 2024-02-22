a194b1819183a0fd26e803fdf64dcd4f0188af31
fuunshi <pranilshrestha2016@gmail.com>
2024-02-22 12:44:03 +0545

    Loading

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const router = createBrowserRouter([
   },
   {
     path:"/bruh/check",
-    element: <LoadingAnimation />
+    element: <ErrorPage />
   }
 ])
 
--- src/components/core/loading.jsx
+++ src/components/core/loading.jsx
@@ -2,7 +2,7 @@ import "./custom.css"
 
 const LoadingAnimation = () => {
   return (
-    <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32 animate-spin"></div>
+    <div className="loader"></div>
   );
 };
 
